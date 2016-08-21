080a1eb9e21ad4303b805724b23fb89ef8b29e37
ndvelasquez <nestorsox35@gmail.com>
2016-08-21 00:00:24 -0400

    se agregaron reportes pdf para trabajadores y estudiantes

--- reporte/pdf_trabajadores.php
+++ reporte/pdf_trabajadores.php
@@ -17,26 +17,28 @@ $pdf->Cell(50, 10, 'Fecha: '.date('d-m-Y').'', 0);
 $pdf->Ln(15);
 $pdf->SetFont('Arial', 'B', 11);
 $pdf->Cell(70, 8, '', 0);
-$pdf->Cell(100, 8, 'LISTADO DE ESTUDIANTES', 0);
+$pdf->Cell(100, 8, 'LISTADO DE TRABAJADORES', 0);
 $pdf->Ln(23);
 $pdf->SetFont('Arial', 'B', 8);
-$pdf->Cell(30, 8, 'Cedula', 1);
+$pdf->Cell(20, 8, 'Cedula', 1);
 $pdf->Cell(40, 8, 'Nombres', 1);
 $pdf->Cell(40, 8, 'Apellidos', 1);
 $pdf->Cell(30, 8, 'Telefono', 1);
 $pdf->Cell(35, 8, 'Fecha nac.', 1);
+$pdf->Cell(30, 8, 'Cargo', 1);
 $pdf->Ln(8);
 $pdf->SetFont('Arial', '', 8);
 //CONSULTA
-$sql = 'SELECT * FROM estudiantes';
+$sql = 'SELECT * FROM trabajadores';
 $rec=mysqli_query($enlace,$sql);
 //SE MUESTRAN EN LA TABLA LOS DATOS CONSULTADOS DE LA BASE DE DATOS//
 while($row=mysqli_fetch_assoc($rec)){
-	$pdf->Cell(30, 8,$row['ci_est'],1);
-	$pdf->Cell(40, 8,$row['nom_est'],1, 0, 'C');
-	$pdf->Cell(40, 8,$row['ape_est'],1, 0, 'C');
-	$pdf->Cell(30, 8,$row['tel_est'],1, 0, 'C');
-	$pdf->Cell(35, 8,$row['fec_nac_est'],1, 0, 'C');
+	$pdf->Cell(20, 8,$row['ci_tra'],1);
+	$pdf->Cell(40, 8,$row['nom_tra'],1, 0, 'C');
+	$pdf->Cell(40, 8,$row['ape_tra'],1, 0, 'C');
+	$pdf->Cell(30, 8,$row['tel_tra'],1, 0, 'C');
+	$pdf->Cell(35, 8,$row['fec_nac_tra'],1, 0, 'C');
+	$pdf->Cell(30, 8,$row['car_tra'],1, 0, 'C');
 	$pdf->Ln(8);
 }
 //MUESTRA EL PDF//
